298d64afa7c2f2642a05a7f1ba2c95fe2b6727a9
Danny Seong <daehoon.seong@gmail.com>
2023-08-26 22:48:58 +1000

    feat: validation and sanitisation - Post, Put

--- router/dweets.js
+++ router/dweets.js
@@ -1,17 +1,34 @@
 const express = require('express');
+const { body } = require('express-validator');
+const dweetsController = require('../controllers/dweet');
+const { validate } = require('../middleware/validator.js');
+
 const router = express.Router();
 
-const dweetsController = require('../controllers/dweet');
-// const dweetsModel = require('../data/model');
+// validation
+// sanitisation
 
+const validateDweet = [
+  body('text')
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage('text should be at least three chracters.'),
+  validate,
+];
+// GET /dweets
+// GET /dweets?username=:username
 router.get('/', dweetsController.getDweets);
 
+// GET /dweets/:id
 router.get('/:id', dweetsController.getDweetsById);
 
-router.post('/', dweetsController.createDweets);
+// POST /dweets
+router.post('/', validateDweet, dweetsController.createDweets);
 
-router.put('/:id', dweetsController.updateDweets);
+// PUT /dweets/:id
+router.put('/:id', validateDweet, dweetsController.updateDweets);
 
+// DELETE /dweets/:id
 router.delete('/:id', dweetsController.deleteDweets);
 
 module.exports = router;
