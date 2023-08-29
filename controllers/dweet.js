20657bee7688b61766a7bdc67adf1996d437bd04
Danny Seong <daehoon.seong@gmail.com>
2023-08-30 09:31:16 +1000

    feat: add authorisation in update/delete - only authorised user can update and delete post.

--- controllers/dweet.js
+++ controllers/dweet.js
@@ -1,37 +1,51 @@
-const dweetsModel = require('../data/dweet');
+const dweetRepository = require('../data/dweet');
 
 async function getDweets(req, res, next) {
   const username = req.query.username;
   const data = await (username
-    ? dweetsModel.getByUsername(username)
-    : dweetsModel.getAll());
+    ? dweetRepository.getByUsername(username)
+    : dweetRepository.getAll());
   return res.status(200).json(data);
   next();
 }
 
 async function getDweetsById(req, res, next) {
   const id = req.params.id;
-  const data = await dweetsModel.getById(id);
+  const data = await dweetRepository.getById(id);
   return res.status(200).json(data);
   next();
 }
 
 async function createDweets(req, res, next) {
   const { text } = req.body;
-  return res.status(201).json(await dweetsModel.create(text, req.userId));
+  return res.status(201).json(await dweetRepository.create(text, req.userId));
   next();
 }
 
 async function updateDweets(req, res, next) {
   const id = req.params.id;
   const text = req.body.text;
-  return res.status(200).json(await dweetsModel.update(id, text));
-  next();
+  const dweet = await dweetRepository.getById(id);
+  if (!dweet) {
+    return res.sendStatus(404);
+  }
+  if (dweet.userId !== req.userId) {
+    return res.sendStatus(403);
+  }
+  const updated = await dweetRepository.update(id, text);
+  return res.status(200).json(updated);
 }
 
 async function deleteDweets(req, res, next) {
   const id = req.params.id;
-  await dweetsModel.remove(id);
+  const dweet = await dweetRepository.getById(id);
+  if (!dweet) {
+    return res.sendStatus(404);
+  }
+  if (dweet.userId !== req.userId) {
+    return res.sendStatus(403);
+  }
+  await dweetRepository.remove(id);
   return res.sendStatus(204);
   next();
 }
